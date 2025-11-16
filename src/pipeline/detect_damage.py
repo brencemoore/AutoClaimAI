51ec655c663d2143b34f6ad0e44bbe2cf0389ddf
brencemoore <brence.exd23@gmail.com>
2025-11-16 17:40:10 -0600

    Created classify_damage and damage_severity functions.

--- src/pipeline/detect_damage.py
+++ src/pipeline/detect_damage.py
@@ -1,8 +1,32 @@
-# This file will be necessary and is in the scope.
-# This file is for:
-# 1. Detecting damages on cars from images.
-# 2. Classifying the severity of damage detected.
-# 3. Detecting the part of the car that is damaged.
+'''
+Uses pre-trained models to classify the type and severity of car damage from a given image.
+'''
 
-# This will be used in report_generator.py and maybe estimate_cost.py
+# From 'https://huggingface.co/beingamit99/car_damage_detection'
 
+def classify_damage(image_path):
+    from transformers import pipeline
+    pipe = pipeline("image-classification", model="beingamit99/car_damage_detection")
+    result = pipe(image_path)
+    print(f'Results: {result}')
+    best = max(result, key=lambda x: x['score'])
+    return best['label']
+
+
+# From 'https://huggingface.co/nezahatkorkmaz/car-damage-level-detection-yolov8'
+
+def damage_severity(image_path):
+    from ultralytics import YOLO
+    from pathlib import Path
+
+    model_path = Path(__file__).resolve().parent.parent / "models" / "car-damage.pt"
+    model = YOLO(model_path)
+    results = model(image_path)
+    
+    # Extract classification probabilities
+    probs = results[0].probs
+    
+    severity = ['minor', 'moderate', 'severe']
+    
+    return severity[probs.top1]
+    
